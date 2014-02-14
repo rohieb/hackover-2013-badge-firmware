9c106604710c691759dd113964911eef627bfe9a
Wintermute <wintermute@hannover.ccc.de>
2014-02-15 00:38:44 +0100

    Größer, Doku.

--- mock-sdl/main.c
+++ mock-sdl/main.c
@@ -2,9 +2,15 @@
 #include "mock/main_loop.h"
 
 #include <emscripten.h>
+#include <stdio.h>
 
 int main(void) {
   mock_display_init();
+
+  puts("Controls:\n"
+       "\n"
+       "   E\n"
+       " S D F   J K");
 
   emscripten_set_main_loop(main_loop, 75, 1);
 
